2edfa9b342f9859e3db8e76f7b90763e6caa6d7e
Aaron Perel <a.a.ron.perel9@gmail.com>
2023-07-10 23:21:59 -0400

    updated auto_browse.py dependencies

--- automated_browsing/auto_browse.py
+++ automated_browsing/auto_browse.py
@@ -11,26 +11,26 @@ from data_processing.google_sheets_processing import read_links_from_google_shee
 webdriver_service = Service('path_to_chromedriver')  # replace 'path_to_chromedriver' with the actual path
 
 def automate_browsing(sheet_name, credentials_file, sheet_key):
-    # Read links from Google Sheets
+    # read links from Google Sheets
     links = read_links_from_google_sheets(sheet_name, credentials_file, sheet_key)
 
-    # Configure Chrome options
+    # configure Chrome options
     chrome_options = Options()
     chrome_options.add_argument('--headless')  # Run Chrome in headless mode
 
-    # Set up the ChromeDriver with the configured options
+    # set up the ChromeDriver with the configured options
     driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
     for link in links:
-        # Open the Instagram link
+        # open the Instagram link
         driver.get(link)
 
-        # Simulate scrolling action
+        # simulate scrolling action
         scroll_element = driver.find_element(By.TAG_NAME, 'body')
-        for _ in range(5):  # Scroll 5 times, adjust the number as needed
+        for _ in range(5):  # scroll 5 times, adjust the number as needed
             scroll_element.send_keys(Keys.PAGE_DOWN)
             WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
 
-    # Close the browser
+    # close the browser
     driver.quit()
 
